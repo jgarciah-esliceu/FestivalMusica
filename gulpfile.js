b56639c88d02d46dd8d197b628dcc6094b56a96f
jgarciah-esliceu <jgarciah@esliceu.net>
2023-01-09 23:39:08 +0100

    empezar 190

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+// CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 function css(done) {
   src("src/scss/**/*.scss") // Identificar el archivo SASS
@@ -11,6 +18,30 @@ function css(done) {
   done(); // Callback que avisa a gulp cuando llegamos al final de la ejecución
 }
 
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3
+  };
+
+  src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin(opciones)))
+    .pipe(dest('build/img'));
+
+  done();
+}
+
+function versionWebp(done) {
+  const opciones = {
+    quality: 50
+  };
+
+  src('src/img/**/*.{png,jpg}')
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'));
+
+  done();
+}
+
 function dev(done) {
   watch("src/scss/**/*.scss", css);
 
@@ -18,4 +49,6 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(imagenes, versionWebp, dev);
